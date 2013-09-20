27e096617b425c223bab59aff1b35fbfa37ab535
David Yip <yipdw@member.fsf.org>
2013-09-20 13:22:17 -0500

    Bump pipeline version.

--- pipeline.py
+++ pipeline.py
@@ -15,7 +15,7 @@ from seesaw.externalprocess import *
 
 from seesaw.util import find_executable
 
-VERSION = "20130917.01"
+VERSION = "20130920.01"
 USER_AGENT = "ArchiveTeam ArchiveBot/%s" % VERSION
 EXPIRE_TIME = 60 * 60 * 48    # 48 hours between archive requests
 WGET_LUA = find_executable('Wget+Lua', "GNU Wget 1.14.lua.20130523-9a5c",
