98b0f2473f9e6cf150558a3f24208a8377e3a27e
David Yip <yipdw@member.fsf.org>
2013-09-23 12:58:27 -0500

    Job query console.

---
+++ console.rb
@@ -0,0 +1,21 @@
+require 'irb'
+require 'redis'
+require 'trollop'
+
+require File.expand_path('../job', __FILE__)
+
+opts = Trollop.options do
+  opt :redis, 'URL of Redis server', :default => ENV['REDIS_URL'] || 'redis://localhost:6379/0'
+end
+
+$redis = Redis.new(:url => opts[:redis])
+
+def job(ident)
+  Job.from_ident(ident, $redis)
+end
+
+def keys
+  $redis.keys
+end
+
+IRB.start
