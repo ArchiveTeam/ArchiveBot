ba94f803867b9c104b13c38411d757c1d90700a7
David Yip <yipdw@member.fsf.org>
2013-09-23 15:39:46 -0500

    Fix syntax error; don't analyze jobs that can't be found.

--- log_analyzer.rb
+++ log_analyzer.rb
@@ -1,9 +1,10 @@
 require File.expand_path('../log_update_listener', __FILE__)
-require File.expand_path'../job', __FILE__)
+require File.expand_path('../job', __FILE__)
 
 class LogAnalyzer < LogUpdateListener
   def on_receive(ident)
     job = ::Job.from_ident(ident, uredis)
-    job.analyze
+
+    job.analyze if job
   end
 end
