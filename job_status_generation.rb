5f1a38cd41030c8baad9d214b60329e4a9f14c32
David Yip <yipdw@member.fsf.org>
2013-10-13 02:18:07 -0500

    ttl never returns nil.

--- job_status_generation.rb
+++ job_status_generation.rb
@@ -21,7 +21,7 @@ module JobStatusGeneration
       rep << "No archive URL currently available."
     end
 
-    if (t = ttl)
+    if (t = ttl) && (t != -1)
       rep << "Eligible for rearchival in #{formatted_ttl(t)}."
     end
 
