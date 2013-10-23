9e91210d311eaa7a81528290e55f8f8438e3715e
David Yip <yipdw@member.fsf.org>
2013-10-22 22:01:38 -0500

    Sort out the vendor code situation.
    It doesn't make sense to have a toplevel vendor directory anymore: for
    example, the redis-lua submodule has no relevance to anything but the
    pipeline program.
    
    This commit also restores the original relative path for the redis-lua
    require.

--- pipeline/archivebot.lua
+++ pipeline/archivebot.lua
@@ -4,7 +4,7 @@ dofile("acceptance_heuristics.lua")
 require('socket')
 
 local json = require('json')
-local redis = require('../vendor/redis-lua/src/redis')
+local redis = require('vendor/redis-lua/src/redis')
 local ident = os.getenv('ITEM_IDENT')
 local rconn = redis.connect(os.getenv('REDIS_HOST'), os.getenv('REDIS_PORT'))
 local aborter = os.getenv('ABORT_SCRIPT')
