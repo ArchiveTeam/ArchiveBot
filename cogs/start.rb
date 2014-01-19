96299c8464ebe0f34e83eb88f7ba8e95da0bd505
Christopher Foo <chris.foo@gmail.com>
2014-01-19 17:28:27 -0500

    Adds TwitterTweeter cog.

--- cogs/start.rb
+++ cogs/start.rb
@@ -9,6 +9,7 @@ require File.expand_path('../../lib/log_update_listener', __FILE__)
 require File.expand_path('../log_analyzer', __FILE__)
 require File.expand_path('../log_trimmer', __FILE__)
 require File.expand_path('../reaper', __FILE__)
+require File.expand_path('../twitter_tweeter', __FILE__)
 
 opts = Trollop.options do
   opt :redis, 'URL of Redis server', :default => ENV['REDIS_URL'] || 'redis://localhost:6379/0'
@@ -17,6 +18,9 @@ opts = Trollop.options do
   opt :db_credentials, 'Credentials for history database (USERNAME:PASSWORD)', :type => String, :default => nil
   opt :log_db, 'URL of CouchDB log database', :default => ENV['LOGDB_URL'] || 'http://localhost:5984/archivebot_logs'
   opt :log_db_credentials, 'Credentials for log database (USERNAME:PASSWORD)', :type => String, :default => nil
+  opt :twitter_username, 'Twitter username', :type => String, :default => nil
+  opt :twitter_config, 'Filename containing Twitter key config', :type => String, 
+    :default => File.expand_path('../twitter_conf.json', __FILE__)
 end
 
 class Broadcaster < LogUpdateListener
@@ -29,6 +33,7 @@ class Broadcaster < LogUpdateListener
     Celluloid::Actor[:log_analyzer].async.process(job)
     Celluloid::Actor[:job_recorder].async.process(job)
     Celluloid::Actor[:log_trimmer].async.process(job)
+    Celluloid::Actor[:twitter_tweeter].async.process(job)
   end
 end
 
@@ -39,6 +44,10 @@ LogTrimmer.supervise_as :log_trimmer, URI(opts[:log_db]),
   opts[:log_db_credentials]
 
 Reaper.supervise_as :reaper, opts[:redis]
+
+if opts[:twitter_username]
+  TwitterTweeter.supervise_as :twitter_tweeter, opts[:redis], opts[:twitter_username], opts[:twitter_config]
+end
 
 ignore_patterns_path = File.expand_path('../../db/ignore_patterns', __FILE__)
 
