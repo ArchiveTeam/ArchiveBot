ef7a88e1bb0e2bfee703adc94a5282a31f68bc29
David Yip <yipdw@member.fsf.org>
2014-02-26 23:19:51 -0600

    GET /pipelines, an endpoint to retrieve pipeline information.
    This'll be used to bootstrap a pipeline dataset, much like how GET
    /logs/recent is used to bootstrap jobs.

---
+++ dashboard/resources/pipeline.rb
@@ -0,0 +1,46 @@
+require 'webmachine'
+
+##
+# A PublicPipelineRecord differs from a plain pipeline record in that it throws
+# away potentially confidential information.
+class PublicPipelineRecord
+  attr_reader :hash
+
+  def initialize(hash)
+    @hash = hash
+  end
+
+  def to_json(*)
+    {
+      'mem_usage' => hash['mem_usage'],
+      'disk_usage' => hash['disk_usage'],
+      'pipeline_id' => hash['id'],
+      'version' => hash['version']
+    }.to_json
+  end
+end
+
+class Pipeline < Webmachine::Resource
+  class << self
+    attr_accessor :redis
+  end
+
+  def content_types_provided
+    [['application/json', :to_json]]
+  end
+
+  def to_json
+    run_query.to_json
+  end
+
+  def run_query
+    r = self.class.redis
+
+    pipeline_ids = r.smembers('pipelines')
+    pipeline_data = r.pipelined do
+      pipeline_ids.map { |p_id| r.hgetall(p_id) }
+    end
+
+    { 'pipelines' => pipeline_data.map { |pd| PublicPipelineRecord.new(pd) } }
+  end
+end
