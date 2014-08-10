89fbbe5af1204c1c264794a088e3807112e8c518
David Yip <yipdw@member.fsf.org>
2014-08-10 18:12:04 -0500

    Eliminate stop_control atexit hook.  #91.
    The pipeline.on_cleanup event was added to seesaw in commit
    658e53ecbaebd7f21f303ac203b048cb0b6cabcd, and was intended to support
    cases where we need to perform deinitialization work in a pipeline.
    Therefore, the atexit hook is probably not necessary.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -29,7 +29,7 @@ from archivebot.seesaw.tasks import GetItemFromQueue, StartHeartbeat, \
     SetWarcFileSizeInRedis, StopHeartbeat, MarkItemAsDone
 
 
-VERSION = "20140807.01"
+VERSION = "20140810.01"
 EXPIRE_TIME = 60 * 60 * 48  # 48 hours between archive requests
 WPULL_EXE = find_executable('Wpull', None, [ './wpull' ])
 PHANTOMJS = find_executable('PhantomJS', '1.9.7',
@@ -188,7 +188,6 @@ pipeline = Pipeline(
 def stop_control():
     control_ref.stop()
 
-atexit.register(stop_control)
 pipeline.on_cleanup += stop_control
 
 # Activate system monitoring.
