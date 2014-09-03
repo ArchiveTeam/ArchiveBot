5929b1bfe6bd057db548eb1f21c90238992f2616
David Yip <yipdw@member.fsf.org>
2014-09-02 20:36:10 -0500

    Bump pipeline version.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -31,7 +31,7 @@ from archivebot.seesaw.tasks import GetItemFromQueue, StartHeartbeat, \
     MarkItemAsDone
 
 
-VERSION = "20140819.03"
+VERSION = "20140902.01"
 EXPIRE_TIME = 60 * 60 * 48  # 48 hours between archive requests
 WPULL_EXE = find_executable('Wpull', None, [ './wpull' ])
 PHANTOMJS = find_executable('PhantomJS', '1.9.7',
