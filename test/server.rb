c5f02c0f2a1ca3773a2ec5a9b9701f3e0ff6df14
David Yip <yipdw@member.fsf.org>
2014-09-09 13:28:18 -0500

    Make the test server exhibit more interesting behavior.
    Specifically, this commit adds multiple links on each test page.  This
    means that concurrency levels > 1 will actually engage multiple workers.

--- test/server.rb
+++ test/server.rb
@@ -1,14 +1,42 @@
 require 'sinatra'
 
-get '/:sec/' do |sec|
-  "<html><head><title>What</title><body><a href='/#{sec}/1.html'>1</a></body></html>"
+str = lambda do |sec, intid|
+    %Q{
+<!doctype html>
+<html>
+<head><title>What</title></head>
+<body>
+  <a href="/#{sec}/#{intid+1}.html">#{intid+1}</a>
+  <a href="/#{sec}/0/#{intid+1}.html">#{intid+1}</a>
+  <a href="/#{sec}/1/#{intid+1}.html">#{intid+1}</a>
+  <a href="/#{sec}/2/#{intid+1}.html">#{intid+1}</a>
+  <a href="/#{sec}/3/#{intid+1}.html">#{intid+1}</a>
+  <a href="/#{sec}/4/#{intid+1}.html">#{intid+1}</a>
+</body>
+</html>
+    }.strip
 end
 
-get /([a-z]+)\/(\d+)\.html/ do |sec, id|
+get '/:sec/' do |sec|
+  str[sec, 0]
+end
+
+get /([a-z0-9]+)\/(\d+)\.html/ do |sec, id|
   intid = id.to_i
 
-  if intid < 20000
-    "<html><head><title>What</title><body><a href='/#{sec}/#{intid+1}.html'>#{intid+1}</a></body></html>"
+  if intid < 500
+    str[sec, intid]
+  else
+    status 404
+    ""
+  end
+end
+
+get /([a-z0-9]+)\/\d+\/(\d+)\.html/ do |sec, id|
+  intid = id.to_i
+
+  if intid < 1000
+    str[sec, intid]
   else
     status 404
     ""
