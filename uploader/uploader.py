d115d14f36fd094e42ff9ff6c500a2c78831d82c
Ivan Kozik <ivan@ludios.org>
2014-09-22 03:46:25 +0000

    Lock the upload directory so that another uploader can't start uploading from it.

--- uploader/uploader.py
+++ uploader/uploader.py
@@ -4,10 +4,36 @@ from __future__ import print_function
 
 import os
 import time
+import fcntl
+import errno
 import subprocess
 import sys
 
 WAIT = 30
+
+class CannotLock(Exception):
+    pass
+
+
+def acquire_lock(fname):
+    """
+    Acquires an exclusive lock on `fname`, which will be truncated to a
+    0-byte file.
+
+    To keep holding the lock, make sure you keep a reference to the
+    returned file object.
+    """
+    f = open(fname, 'wb')
+    try:
+        fcntl.lockf(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except OSError as e:
+        if e.errno not in (errno.EACCES, errno.EAGAIN):
+            # Error was not locking-related, so re-raise.
+            # See http://docs.python.org/library/fcntl.html#fcntl.lockf
+            raise
+        raise CannotLock(fname)
+    return f
+
 
 def main():
     if len(sys.argv) > 1:
@@ -20,6 +46,12 @@ def main():
     url = os.environ.get('RSYNC_URL')
     if url == None:
         raise RuntimeError('RSYNC_URL not set')
+
+    try:
+        # Do not remove this local even if pyflakes complains about it
+        lockfile = acquire_lock(os.path.join(directory, ".uploader.lock"))
+    except CannotLock:
+        raise RuntimeError("Another uploader is uploading from %s" % (directory,))
 
     while True:
         fnames = sorted(list(f for f in os.listdir(directory) if not f.startswith('.') and (f.endswith('.warc.gz') or f.endswith('.json'))))
