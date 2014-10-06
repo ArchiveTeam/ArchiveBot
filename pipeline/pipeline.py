dba60a42e0586fb140d666407645f95dc6be0778
Ivan Kozik <ivan@ludios.org>
2014-10-06 22:59:06 +0000

    Refuse to start outside screen or tmux

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -49,6 +49,9 @@ assert PHANTOMJS, 'PhantomJS %s was not found.' % PHANTOMJS_VERSION
 assert 'RSYNC_URL' in env, 'RSYNC_URL not set.'
 assert 'REDIS_URL' in env, 'REDIS_URL not set.'
 
+assert 'TMUX' in env or 'STY' in env or env.get('NO_SCREEN') == "1", \
+        "Refusing to start outside of screen or tmux, set NO_SCREEN=1 to override"
+
 if StrictVersion(seesaw.__version__) < StrictVersion("0.1.8b1"):
     raise Exception(
         "Needs seesaw@python3/development version 0.1.8b1 or higher. "
