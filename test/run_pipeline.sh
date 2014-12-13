2db5c6fae3cb49fae7094d73dbefff766cffca1c
Christopher Foo <chris.foo@gmail.com>
2014-12-13 18:43:24 -0500

    test/run_pipeline: Override python3 to python3.4

--- test/run_pipeline.sh
+++ test/run_pipeline.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+export PATH="/tmp/bin/:$PATH"
+export REDIS_URL='redis://localhost:6379/0'
+export RSYNC_URL='/tmp/rsync/' LOG_CHANNEL='updates'
+export NO_SCREEN=1 FINISHED_WARCS_DIR=/tmp/warc/
+
 mkdir -p /tmp/rsync/
 mkdir -p /tmp/warc/
 
-REDIS_URL='redis://localhost:6379/0' \
-  RSYNC_URL='/tmp/rsync/' LOG_CHANNEL='updates' \
-  NO_SCREEN=1 FINISHED_WARCS_DIR=/tmp/warc/ \
-  ~/.local/bin/run-pipeline3 --disable-web-server --concurrent 1 \
+# This will affect the wpull exe wrapper
+mkdir -p /tmp/bin/
+ln -s /usr/bin/python3.4 /tmp/bin/python3
+
+echo -n 'Using '
+which python3
+
+~/.local/bin/run-pipeline3 --disable-web-server --concurrent 1 \
     pipeline/pipeline.py TestUser
