cdb3a71ff82298f2249974384a5020c0283c7a8c
Christopher Foo <chris.foo@gmail.com>
2015-02-21 19:59:54 -0500

    pipeline: Fail early when sparse file not supported

--- pipeline/archivebot/dupespotter/dupes.py
+++ pipeline/archivebot/dupespotter/dupes.py
@@ -1,13 +1,28 @@
 # encoding=utf-8
 '''Duplicate page content database.'''
 
+import os
+import tempfile
+
 
 class DupesOnDisk(object):
     def __init__(self, filename):
         import lmdb
-        # TODO: lmdb needs a sparse file; fail early instead of using 1TB
+        # lmdb needs a sparse file; fail early instead of using 1TB
         # of disk on filesystems with no sparse file support
-        for map_size in (1024*1024*1024*1024, 2**31-1):
+        if not self.fs_supports_sparse_files():
+            if not os.environ.get('DUPESSPOTTER_SMALL_FILES'):
+                raise Exception(
+                    'Sparse file not supported. '
+                    'Use DUPESSPOTTER_SMALL_FILES=1 to use small files '
+                    'but may crash. Not for production use.'
+                )
+
+            sizes = (1024 ** 3,)
+        else:
+            sizes = (1024 * 1024 * 1024 * 1024, 2 ** 31 - 1)
+
+        for map_size in sizes:
             try:
                 self._env = lmdb.open(
                     filename,
@@ -31,6 +46,12 @@ class DupesOnDisk(object):
     def set_old_url(self, digest, url):
         with self._env.begin(write=True) as txn:
             return txn.put(digest, url.encode("utf-8"))
+
+    def fs_supports_sparse_files(self):
+        with tempfile.NamedTemporaryFile(dir=os.getcwd()) as file:
+            file.truncate(1000000)
+
+            return os.stat(file.name).st_blocks == 0
 
 
 class DupesInMemory(object):
