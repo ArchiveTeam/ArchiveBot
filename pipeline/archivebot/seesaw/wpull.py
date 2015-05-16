1838f2f1709ba74a4777f25e3772635a7de6170c
Christopher Foo <chris.foo@gmail.com>
2015-05-15 21:35:14 -0400

    pipeline: Raise disk/mem monitor to 500m/50m. Set max redirect to 8.
    Raise the monitor disk and memory args to 500 MB and 50 MB respectively.
    
    Set the max redirect to 8 instead of default 20 so it gets around those
    broken servers faster. Wget's 20 is very conservative.
    
    Bump version.

--- pipeline/archivebot/seesaw/wpull.py
+++ pipeline/archivebot/seesaw/wpull.py
@@ -51,8 +51,9 @@ def make_args(item, default_user_agent, wpull_exe, phantomjs_exe, finished_warcs
         '--strip-session-id',
         '--escaped-fragment',
         '--session-timeout', '21600',
-        '--monitor-disk', '50m',
-        '--monitor-memory', '10k',
+        '--monitor-disk', '500m',
+        '--monitor-memory', '50m',
+        '--max-redirect', '8',
     ]
 
     if item['url'].startswith("http://www.reddit.com/") or \
