5ba0695b950f6fd770c49af4b454ba795a2d675d
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2019-08-19 01:13:56 +0000

    More tests

---
+++ test/bottest3.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+import time
+import signal
+
+
+script_dir = os.path.dirname(__file__)
+bot_script = os.path.join(script_dir, 'run_bot.sh')
+subprocess.run([bot_script], timeout = 30)
