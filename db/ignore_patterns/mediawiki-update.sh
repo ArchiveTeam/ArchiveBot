83cc8d8a1bfee4c5b873c53ffcad708ce43e627d
Manuel Groß <manu@fairydust.email>
2026-05-15 13:52:52 +0200

    ignore_patterns: Add mediawiki-tr (Turkish)

--- db/ignore_patterns/mediawiki-update.sh
+++ db/ignore_patterns/mediawiki-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for lang in Ar Bg Bs Ca Cs De El Eo Es Fa Fi Fr He It Ja Ka Ko Li Nl Pl Pt Ro Ru Sd Sl Sq Sv Uk Zh
+for lang in Ar Bg Bs Ca Cs De El Eo Es Fa Fi Fr He It Ja Ka Ko Li Nl Pl Pt Ro Ru Sd Sl Sq Sv Tr Uk Zh
 do
 	echo "${lang}"
 	./mediawiki-lang.py mediawiki.json <(curl --silent --location https://github.com/wikimedia/mediawiki/raw/1.34.2/languages/messages/Messages${lang}.php) mediawiki-${lang,,} >mediawiki-${lang,,}.json
